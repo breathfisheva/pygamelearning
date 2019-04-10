f8598b1c06d419b567fb9b8d5dc682eccf5cf0bc
breathfisheva <hongliu_goodluck@sina.com>
2019-04-10 17:31:47 +0800

    day8 bounce

---
+++ day8.py
@@ -0,0 +1,42 @@
+'''
+遇到边界反弹
+
+'''
+background_image_filename = 'sushiplate.jpg'
+sprite_image_filename = 'fugu.png'
+
+import pygame
+from pygame.locals import *
+from sys import exit
+
+pygame.init()
+
+screen = pygame.display.set_mode((640, 480), 0, 32)
+
+background = pygame.image.load(background_image_filename).convert()
+sprite = pygame.image.load(sprite_image_filename)
+
+clock = pygame.time.Clock()
+
+x = 0
+speed = 250
+
+while True:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            exit()
+
+    screen.blit(background, (0,0))
+    screen.blit(sprite, (x, 100))
+
+    time_passed = clock.tick()
+    time_passed_seconds = time_passed / 1000.0
+
+    distance_moved = time_passed_seconds * speed
+    x += distance_moved
+
+    if x > 640-sprite.get_width() or x < 0:  #如果到屏幕最右边或者最左边
+        speed = -1 * speed #反弹是把速度取反了而已
+
+    pygame.display.update()
+
